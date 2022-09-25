a78b4840b0c11cfc7c03172d278408f4617445fc
Hassan Usman <husmanjah@gmail.com>
2022-09-25 23:27:20 +0100

    error fixed

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,21 @@
 #include "main.h"
-
 /**
- * print_last_digit - print last digit of an integer
- * @n: the integer to print the last digit of
+ * print_last_digit - prints the last digit of a number
+ * @ld: the last digit
  *
- * Return: Value of last digit
+ * Return: the value of the last digit
  */
-int print_last_digit(int n)
+int print_last_digit(int ld)
 {
-	if ((n % 10) < 0)
-		n *= -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	if (ld > 0)
+	{
+		ld = ld % 10;
+		_putchar(ld + '0');
+	}
+	else
+	{
+		ld = ld % 10 * -1;
+		_putchar(ld + '0');
+	}
+	return (ld);
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-This project has a series of files containing user made functions.
+function nested
